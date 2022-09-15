0972bdd1eea774afe5cba9e5093d65b0100daf24
Tra Quang Minh Thong <tqmthong@gmail.com>
2022-09-15 16:56:39 +0800

    Update erd and relational schema 15/9

--- project_1.sql
+++ project_1.sql
@@ -3,32 +3,66 @@ CREATE	TABLE 	Countries(
 	tax_code	INTEGER NOT NULl,
 );
 
-CREATE	TABLE	Users(
+CREATE	TABLE	Creators(
 	uname 	VARCHAR(50) NOT NULL,
 	email 	VARCHAR(50) UNIQUE NOT NULL,
 	card_1	VARCHAR(20) NOT NULL,
 	card_2 	VARCHAR(20),
-	utype 	VARCHAR(10) CHECK (
-		utype = 'creator'
-		OR utype = 'backer'
-	),
-	street_name		VARCHAR(50),
-	house_number	VARCHAR(30),
-	zip_code		VARCHAR(30),
-	cname 			VARCHAR(20) REFERENCES Countries(cname),
+	country VARCHAR(20) REFERENCES Countries(cname),
 	PRIMARY KEY (uname, email),
 	
 );
 
 CREATE 	TABLE	Projects(
-	pname 			VARCHAR(100) PRIMARY KEY,
+	pid 			SERIAL,
+	pname 			VARCHAR(100),
 	ptype 			VARCHAR(40),
-	reward_level 	VARCHAR(10) CHECK (
-		reward_level = 'Platinum'
-		OR reward_level = 'Gold'
-		OR reward_level = 'Silver'
-		OR reward_level = 'Bronze'
-	),
+	reward_level 	VARCHAR(20) NOT NULL,
+	funding_goal	BIGINT,
+	deadline 		DATE,
+	creator VARCHAR(50) NOT NULL,
+	backers VARCHAR()
+	
+	PRIMARY KEY (pid, reward_level),
+	FOREIGN KEY creator REFERENCES Creators(uname, email) ON DELETE CASCADE ON UPDATE CASCADE, 
 	
 );
 
+CREATE	TABLE	Backers(
+	uname 	VARCHAR(50) NOT NULL,
+	email 	VARCHAR(50) UNIQUE NOT NULL,
+	card_1	VARCHAR(20) NOT NULL,
+	card_2 	VARCHAR(20),
+	street_name		VARCHAR(50) NOT NULL,
+	house_number	VARCHAR(30) NOT NULL,
+	zip_code		INTEGER NOT NULL,
+	country 		VARCHAR(20) REFERENCES Countries(cname),
+-- 	projects		SERIAL UNIQUE,
+	
+-- 	UNIQUE(projects, back_level),
+	PRIMARY KEY (uname, email),
+-- 	FOREIGN KEY (projects, back_level) REFERENCES Projects(pid, reward_level),
+	
+);
+
+CREATE TABLE Employees(
+	eid 	INTEGER UNIQUE NOT NULL,
+	ename 	VARCHAR(50) NOT NULL, 
+);
+
+CREATE TABLE Funding(
+	pname 	VARCHAR(100),
+	bname 	VARCHAR(50),
+	reward_level 	VARCHAR(20) NOT NULL,
+	email 	VARCHAR(50) NOT NULL,
+	
+	PRIMARY KEY (pname, bname),
+-- 	UNIQUE (pname, bname, reward_level),
+	FOREIGN KEY (pname, reward_level) REFERENCES Projects(pname, reward_level) ON UPDATE CASCADE,
+	FOREIGN KEY (bname, email) REFERENCES Backers(uname, email),
+)
+
+CREATE TABLE Refund(
+	pname 	VARCHAR(100),
+	bname 	VARCHAR(50),
+)
