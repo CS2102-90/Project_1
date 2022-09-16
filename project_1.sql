040038f20d6fb7971825b6854135b6078194fda3
Tra Quang Minh Thong <tqmthong@gmail.com>
2022-09-17 00:09:50 +0800

    Implement update_project relationship, add cemail to PK of Project to prevent simultaneous update

--- project_1.sql
+++ project_1.sql
@@ -1,66 +1,122 @@
 CREATE	TABLE 	Countries(
 	cname 		VARCHAR(20) PRIMARY KEY,
-	tax_code	INTEGER NOT NULl,
+	tax_code	INTEGER NOT NULl	
+);
+
+CREATE TABLE Employees(
+	eid 	INTEGER PRIMARY KEY,
+	ename 	VARCHAR(50) NOT NULL, 
+	salary  INTEGER NOT NULL
 );
 
 CREATE TABLE Users(
-	uname 	VARCHAR(50) NOT NULL,
-	email 	VARCHAR(50) PRIMARY KEY,
-	card_1	VARCHAR(20) NOT NULL,
-	card_2 	VARCHAR(20),
+	email 		VARCHAR(50) PRIMARY KEY,
+	uname 		VARCHAR(50) NOT NULL,
+	card_1		VARCHAR(20) NOT NULL,
+	card_2 		VARCHAR(20),
 );
 
 CREATE	TABLE	Creators(
-	email 	VARCHAR(50) UNIQUE NOT NULL,
-	country VARCHAR(20),
-	FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (country) REFERENCES Countries(cname) ON UPDATE CASCADE ON DELETE SET NULL,	
-);
+	email 	VARCHAR(50) PRIMARY KEY,
+	country VARCHAR(20) NOT NULL,
 
-CREATE 	TABLE	Projects(
-	pid 			SERIAL,
-	pname 			VARCHAR(100),
-	ptype 			VARCHAR(40),
-	reward_level 	VARCHAR(20) NOT NULL,
-	funding_goal	BIGINT,
-	deadline_date 	DATE,
-	deadline_time 	TIME,
-	cemail VARCHAR(50) NOT NULL,
-	backers VARCHAR()
-	
-	PRIMARY KEY (pid, reward_level),
-	FOREIGN KEY cemail REFERENCES Creators(email) ON DELETE SET NULL ON UPDATE CASCADE, 
-	
+	FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (country) REFERENCES Countries(cname) ON DELETE SET NULL ON UPDATE CASCADE	
 );
 
 CREATE	TABLE	Backers(
-	email 	VARCHAR(50) PRIMARY KEY,
+	email 			VARCHAR(50) PRIMARY KEY,
 	street_name		VARCHAR(50) NOT NULL,
 	house_number	VARCHAR(30) NOT NULL,
 	zip_code		INTEGER NOT NULL,
 	country 		VARCHAR(20) REFERENCES Countries(cname),
 	
-	FOREIGN KEY (email) REFERENCES Users(email),	
+	FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Employees(
-	eid 	INTEGER UNIQUE NOT NULL,
-	ename 	VARCHAR(50) NOT NULL, 
+CREATE 	TABLE	Projects(
+	pid 			SERIAL,
+	ptype 			VARCHAR(40),
+	reward_level 	VARCHAR(20) NOT NULL,
+	min_funding 	INTEGER,
+	funding_goal	INTEGER,
+	deadline_date 	DATE,
+	deadline_time 	TIME,
+	cemail 			VARCHAR(50) NOT NULL,
+	bemail 	 		VARCHAR(),
+	create_date  	DATE,
+	
+	UNIQUE(pid, reward_level),
+	UNIQUE(pid, deadline_date),
+	PRIMARY KEY (pid, reward_level, min_funding, deadline_date, cemail),
+	FOREIGN KEY cemail REFERENCES Creators(email) ON DELETE SET NULL ON UPDATE CASCADE, 
 );
 
 CREATE TABLE Funding(
-	pname 	VARCHAR(100),
+	pid 			INTEGER,
 	reward_level 	VARCHAR(20) NOT NULL,
-	min_funding 	BIGINT,
-	bemail 	VARCHAR(50) NOT NULL,
-	fund 	INTEGER NOT NULL,
+	min_funding 	INTEGER
+	amount 			INTEGER,
+	cemail			VARCHAR(50) NOT NULL,
+	bemail 			VARCHAR(50) NOT NULL,
+	fund 			INTEGER NOT NULL,
+	ddl 			DATE,
+	fund_date 		DATE,
 	
-	PRIMARY KEY (pname, bemail),
-	FOREIGN KEY (pname, reward_level, min_funding) REFERENCES Projects(pname, reward_level, funding_goal) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (bemail) REFERENCES Backers(email) ON UPDATE CASCADE ON DELETE SET NULL,
-)
+	PRIMARY KEY (pid, bemail, ddl),
+	FOREIGN KEY (pid, reward_level, min_funding, ddl) REFERENCES Projects(pid, reward_level, min_funding, deadline_date, cemail) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (bemail) REFERENCES Backers(email) ON DELETE SET NULL ON UPDATE CASCADE,
+	CHECK (amount >= min_funding),
+	CHECK (fund_date <= DATEADD(day, 0, ddl))
+);
+
+CREATE TABLE Update_project(
+	pid 			INTEGER,
+	cemail			VARCHAR(50) NOT NULL,
+	reward_level	VARCHAR(20) NOT NULL,
+	min_funding 	INTEGER,
+	deadline_date 	DATE,
+	update_time		TIME,
+	update_date		DATE,
+	
+	PRIMARY KEY (pid, reward_level, min_funding, deadline_date, cemail, update_time, update_date),
+	FOREIGN KEY (pid, reward_level, min_funding, deadline_date, cemail) REFERENCES Projects(pid, reward_level, min_funding, deadline_date, cemail) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 CREATE TABLE Refund(
-	pname 	VARCHAR(100),
-	bname 	VARCHAR(50),
-)
+	pid 		INTEGER NOT NULL,
+	bemail 		VARCHAR(50) NOT NULL,
+	ddl 		DATE NOT NULL,
+	refund_date DATE NOT NULL,
+
+	PRIMARY KEY (pid, bemail, refund_date),
+	FOREIGN KEY (pid, bemail, ddl) REFERENCES Funding(pid, bemail, ddl) ON DELETE CASCADE ON UPDATE CASCADE,
+	CHECK (refund_date <= DATEADD(day, -90, ddl))
+);
+
+CREATE TABLE Check_Refund(
+	eid				INTEGER,
+	pid 			INTEGER NOT NULL,
+	bemail 			VARCHAR(50) NOT NULL,
+	refund_date 	DATE NOT NULL,
+	refund_status 	VARCHAR(50) 
+	
+	CHECK (
+		(refund_status = 'APPROVED') OR 
+		(refund_status = 'REJECT')
+	),
+	PRIMARY KEY (eid, pid, bemail),
+	FOREIGN KEY (eid) REFERENCES Employees(eid) ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY (pid, bemail, refund_date) REFERENCES Refund(pid, bemail, refund_date) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Verify(
+	eid 			INTEGER UNIQUE,
+	uemail 			VARCHAR(50) NOT NULL,
+	vertify_status  VARCHAR(50),
+	vertify_date	DATE,
+
+	PRIMARY KEY(eid, uemail),
+	FOREIGN KEY(eid) REFERENCES Employees(eid) ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY(uemail) REFERENCES Users(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
